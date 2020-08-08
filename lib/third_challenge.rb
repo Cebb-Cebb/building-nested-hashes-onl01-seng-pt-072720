eedb4a0f8797514b2a4c39b14ca38df36f267174
hersson ceballos <herssonceballos@gmail.com>
2020-08-08 23:13:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {name: "Lady Capulet", 
         #your key/value pairs here
         },
       :matriarch => {
